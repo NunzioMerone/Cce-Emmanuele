136cd30a68ce0cdd389f33eb5bb2626e7fbe4f74
NunzioMerone <n.merone@software-inside.it>
2025-12-11 16:44:26 +0100

    rete

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,24 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
+  },
+  server: {
+    host: true,
+    port: 5173,
+  },
+  preview: {
+    host: true,
+    port: 4173,
+  },
 });
