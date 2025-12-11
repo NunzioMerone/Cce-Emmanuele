d6b84d08b352c8017fa06e15b4bf8d1a16e9ceb4
NunzioMerone <n.merone@software-inside.it>
2025-12-11 16:50:39 +0100

    new

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Cce-Emmanuele/", // Nome del tuo repository
+  base: "/", // Cambia in base al tuo setup - usa '/' se hai dominio custom
   build: {
     outDir: "dist",
     assetsDir: "assets",
