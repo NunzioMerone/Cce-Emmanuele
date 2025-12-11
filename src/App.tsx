a5acbc5a73d0ade6b60b81bd289d66daac1b6938
NunzioMerone <n.merone@software-inside.it>
2025-12-11 16:57:53 +0100

    Fix TypeScript unused import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,37 @@
 import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { AppRoutes } from "./router/routes";
+import { HomePage } from "./pages/HomePage";
+import { MinistriesPage } from "./pages/MinistriesPage";
+import { EventsPage } from "./pages/EventsPage";
+import { MediaPage } from "./pages/MediaPage";
+import { AboutPage } from "./pages/AboutPage";
+import { ContactPage } from "./pages/ContactPage";
+import { AdminLoginPage } from "./pages/AdminLoginPage";
+import { AdminPage } from "./pages/AdminPage";
+import { ProtectedRoute } from "./components/admin/ProtectedRoute";
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/ministeri" element={<MinistriesPage />} />
+      <Route path="/eventi" element={<EventsPage />} />
+      <Route path="/media" element={<MediaPage />} />
+      <Route path="/chi-siamo" element={<AboutPage />} />
+      <Route path="/contatti" element={<ContactPage />} />
+      <Route path="/admin/login" element={<AdminLoginPage />} />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute>
+            <AdminPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
