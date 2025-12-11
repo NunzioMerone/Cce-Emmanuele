62ebfe7fe7428c79f34bcaf9ab37af9fd8153d9d
NunzioMerone <n.merone@software-inside.it>
2025-12-11 17:44:54 +0100

    Switch to HashRouter for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
-// Usa basename solo in produzione
-const isDevelopment = import.meta.env.DEV;
-const basename = isDevelopment ? "/" : "/Cce-Emmanuele";
-
+// Usa HashRouter per GitHub Pages
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
